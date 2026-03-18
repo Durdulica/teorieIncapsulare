a8bb9ad6dde7cd2373b40eafad662b23501fe504
Stefan Scarlat <stefanel.scarlat@gmail.com>
2026-03-18 20:57:23 +0200

    masina

---
+++ Masina.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//todo:Constructori
+// este o metoda ce se apeleaza automat la instantiere
+// se poate supraincarca
+//nu are tip returnat 
+//are acelasi nume ca si clasa
+// daca nu definim un constructor csharp "ne doneaza" unul automat
+namespace teorieIncapsulare
+{
+    public class Masina
+    {
+        private string marca;
+        private string model;
+        private int anFabricare;
+        private int caiPutere;
+        private int greutate;
+        private string culoare;
+
+
+        public Masina()
+        {
+            Console.WriteLine("constructor fara parametrii");
+        }
+
+        public Masina(string marca, string model, int anFabricare, int caiPutere, int greutate, string culoare)
+        {
+            Marca = marca;
+            Model = model;
+            AnFabricare = anFabricare;
+            CaiPutere = caiPutere;
+            Greutate = greutate;
+            Culoare = culoare;
+        }
+
+        public string Marca
+        {
+            get { return marca; } set
+            {
+
+                bool isEmpty = value.Length == 0;
+
+                if (isEmpty)
+                {
+                    throw new ArgumentException("Marca nu poate fi empty");
+                }
+                marca = value;
+            }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                bool isEmpty = value.Length == 0;
+
+                if (isEmpty)
+                {
+                    throw new ArgumentException("Modelul nu poate fi empty");
+                }
+                model = value;
+            }
+        }
+
+        public int AnFabricare
+        {
+            get { return anFabricare; }
+            set
+            {
+                if (value < 0 || value > 2026)
+                {
+                    throw new ArgumentException("Anul fabricarii nu este corect");
+                }
+                anFabricare = value;
+            }
+        }
+
+        public int CaiPutere
+        {
+            get { return caiPutere; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Puterea nu poate fi negativa");
+                }
+                caiPutere = value;
+            }
+        }
+
+        public int Greutate
+        {
+            get { return greutate; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Greutatea nu poate fi negativa");
+                }
+                greutate = value;
+            }
+        }
+
+        public string Culoare
+        {
+            get { return culoare; }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Culoarea nu poate fi empty");
+                }
+                culoare = value;
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using teorieIncapsulare;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Masina m1 = new Masina();
+        //m1.marca = "BMW";
+        //m1.model = "M4";
+        //m1.anFabricare = 2007;
+        //m1.culoare = "negru";
+        //m1.caiPutere = 200;
+        //m1.greutate = 1350;
+
+        //    Masina m2 = new();
+        //    m2.marca = "Skoda";
+        //    m2.model = "Yeti";
+        //    m2.anFabricare = 2013;
+        //    m2.culoare = "bej";
+        //    m2.caiPutere = 135;
+        //    m2.greutate = 1560;
+
+        //    Masina m3 = new();
+        //    m3.marca = "Dacia";
+        //    m3.model = "Logan";
+        //    m3.model = "Logan";
+        //    m3.anFabricare = 2004;
+        //    m3.culoare = "galben";
+        //    m3.caiPutere = 110;
+        //    m3.greutate = 1050;
+
+        //    Masina m4 = new();
+        //    m4.marca = "Honda";
+        //    m4.model = "CRV";
+        //    m4.anFabricare = 2020;
+        //    m4.culoare = "visiniu";
+        //    m4.caiPutere = 165;
+        //    m4.greutate = 1680;
+
+        //    Masina m5 = new();
+        //    m5.marca = "Ford";
+        //    m5.model = "Focus";
+        //    m5.anFabricare = 2006;
+        //    m5.culoare = "albastru";
+        //    m5.caiPutere = 120;
+        //    m5.greutate = 1100;
+
+        //    m5.anFabricare = -9;
+        //
+
+        try
+        {
+            Masina x = new Masina("BMW", "", 2000, 150, 1245, "gri");
+        }
+        catch(ArgumentException ex) { 
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
